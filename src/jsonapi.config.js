2f1f3e6b0601cbbf180cd85592249226543508c5
Julien Dussart <julien.dussart@leikir-studio.fr>
2016-10-28 15:58:34 +0200

    remove config of log provider

--- src/jsonapi.config.js
+++ src/jsonapi.config.js
@@ -2,9 +2,6 @@
   'use strict';
 
   angular.module('angular-jsonapi')
-  .config(function($logProvider) {
-    $logProvider.debugEnabled(false);
-  })
   .run(function(validateJS, $q) {
     validateJS.Promise = $q;
   });
